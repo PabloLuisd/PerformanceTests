8b374254714d69240492651738848a948ded62e4
PabloLuisd_ <dasilvapabloluis21@gmail.com>
2026-06-19 23:51:50 -0300

    Troca de sistema e primeiros testes

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,41 @@
 from locust import HttpUser, task, between
 
 
-class SauceDemoUser(HttpUser):
+class DummyJsonUser(HttpUser):
 
-    host = "https://www.saucedemo.com"
+    host = "https://dummyjson.com"
     wait_time = between(1, 3)
 
-    @task(3)
-    def home_page(self):
-        self.client.get("/")
+    @task
+    def listar_produtos(self):
+        self.client.get("/products", name="Listar Produtos")
 
-    @task(1)
-    def login_page(self):
-        self.client.get("/")
+    @task
+    def consultar_produto(self):
+        self.client.get("/products/1", name="Consultar Produto")
+
+    @task
+    def listar_usuarios(self):
+        self.client.get("/users", name="Listar Usuários")
+
+    @task
+    def login(self):
+        self.client.post(
+            "/auth/login",
+            json={
+                "username": "emilys",
+                "password": "emilyspass"
+            },
+            name="Login"
+        )
+
+    @task
+    def criar_postagem(self):
+        self.client.post(
+            "/posts/add",
+            json={
+                "title": "Teste Locust",
+                "userId": 1
+            },
+            name="Criar Postagem"
+        )
